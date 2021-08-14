0fae8ff52e11d117983a65f3c32008a3a4f9f08f
xiaochunei <476734965@qq.com>
2021-08-14 17:17:09 +0800

    代码提价   --xiao

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,9 +8,9 @@ module.exports = {
   deploy: {
     production: {
       user: 'root',
-      host: '47.99.134.126',
+      host: '121.5.28.174',
       ref: 'origin/main',
-      repo: 'git@git.zhlh6.cn:newbee-ltd/vue3-admin.git',
+      repo: 'git@github.com:chunleixue/myapp.git',
       path: '/workspace/vue3-admin',
       'post-deploy': 'git reset --hard && git checkout main && git pull && npm i --production=false && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
       env: {
